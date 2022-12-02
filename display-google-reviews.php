57413844560b0406908799f30c5319e3e53f302d
Kevin Steve Maningo <kevinsteve.maningo@gmail.com>
2022-12-02 16:43:34 +0800

    Updated to version 1.1

--- display-google-reviews.php
+++ display-google-reviews.php
@@ -4,13 +4,13 @@
  *
  * @package       COPYRIGHTS
  * @author        Kevin Steve Maningo
- * @version       1.0
+ * @version       1.1
  *
  * @wordpress-plugin
  * Plugin Name:   Display Google Reviews
  * Plugin URI:    https://www.kevinstevemaningo.com
  * Description:   Display Google Reviews
- * Version:       1.0
+ * Version:       1.1
  * Author:        Kevin Steve Maningo
  * Author URI:    https://www.kevinstevemaningo.com
  * GitHub Plugin URI: kevinsteve5810/display-google-reviews
